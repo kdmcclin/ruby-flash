0c850c71e270815bd11186b03cf2d68fce3ce76f
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-09-05 15:27:19 -0400

    play debugging

--- controller.rb
+++ controller.rb
@@ -10,8 +10,24 @@ class Controller
 	end
 
 	def play
-		
-	end
+		@deck.shuffle
+		unless @deck.end_of_game?
+			
+			answer, question = @deck.grab_a_card
+			@input = @view.prompt_question(question)
 
+			if @input == 'exit'
+				break
+ 			elsif can_proceed?(@input) == :correct
+ 				@view.prompt_correct
+ 			elsif can_proceed?(@input) == :too_many_guesses
+ 				@view.prompt_too_many
+ 				@view.prompt_correct_answer(answer)
+ 			else can_proceed?(@input) == :incorrect
+ 				@view.prompt_incorrect
+ 			end		
+		end
+		@view.print_stats(@deck.get_num_correct, @deck.get_num_incorrect)
+	end
 
 end
--- view.rb
+++ view.rb
@@ -9,20 +9,38 @@ class View
 		puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition."
 	end
 
-	def prompt_question(definition) #takes the definition and prints it out. Returns input from the user
+	def prompt_question(question) #takes the question and prints it out. Returns input from the user
 		puts "Definition: "
-		puts "- #{definition.capitalize}\n\n"
+		puts "- #{question.capitalize}\n\n"
 		print "Guess: "
 		gets.chomp
 	end
 
-	def prompt_incorrect(definition)
+	def prompt_incorrect(question)
 		puts "Incorrect! Try again.\n\n"
-		prompt_question(definition)
+	end
+
+	def prompt_too_many
+		puts "Incorrect! No more guesses available."		
+	end
+
+	def prompt_correct_answer(answer)
+		puts "The correct answer: #{answer}"	
 	end
 
 	def prompt_correct
 		puts "Correct!"
+	end
+
+	def print_stats(correct, incorrect)
+		line
+		puts "Number of correct: #{correct}"
+		puts "Number of incorrect: #{incorrect}"
+		line
+	end
+
+	def line
+		puts "----------"
 	end
 
 end
