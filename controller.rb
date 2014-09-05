691f786bc926c67d647659e644297e2936ceae60
Michelle Chu & Nicolle Quintero <pair+michellechu77.nicollequintero@devbootcamp.com>
2014-09-05 17:15:22 -0400

    challenge completed

--- controller.rb
+++ controller.rb
@@ -1,33 +1,43 @@
 require_relative 'view'
-
-
+require_relative 'Deck_and_Card_Classes'
+require 'pry'
 class Controller
-
+  attr_reader	:view, :deck
 	def initialize
 		@view = View.new
-		@deck = Deck.new
+		@deck = Deck.new("Ruby Deck",'ruby_deck.txt')
 		play
 	end
 
 	def play
+		view.print_intro
 		@deck.shuffle
-		unless @deck.end_of_game?
-			
-			answer, question = @deck.grab_a_card
-			@input = @view.prompt_question(question)
 
-			if @input == 'exit'
-				break
- 			elsif can_proceed?(@input) == :correct
- 				@view.prompt_correct
- 			elsif can_proceed?(@input) == :too_many_guesses
- 				@view.prompt_too_many
- 				@view.prompt_correct_answer(answer)
- 			else can_proceed?(@input) == :incorrect
- 				@view.prompt_incorrect
- 			end		
+		while !deck.end_of_game?
+			answer, question = deck.grab_a_card
+			view.prompt_question(question) unless deck.cards[0].guesses >0
+			input = view.get_input
+
+			if input == 'quit'
+				view.print_stats(deck.get_num_correct, deck.get_num_incorrect-1)
+				exit
+			end
+
+			next_step = deck.can_proceed?(input)
+ 			if next_step == :correct
+ 				view.prompt_correct
+ 			elsif next_step == :too_many_guesses
+ 				view.prompt_too_many
+ 				view.prompt_correct_answer(answer)
+ 			else next_step == :incorrect
+ 				view.prompt_incorrect
+ 			end
 		end
-		@view.print_stats(@deck.get_num_correct, @deck.get_num_incorrect)
+
+		# p @deck.cards
+		view.print_stats(deck.get_num_correct, deck.get_num_incorrect)
 	end
 
 end
+
+Controller.new
--- view.rb
+++ view.rb
@@ -1,35 +1,58 @@
 require 'pry'
 
+ def print_by_letter(string)
+    string.chars.each {|char| sleep 0.02; print char}
+    puts ""
+  end
+
 class View
 
-	def initialize 
+	def initialize
 	end
-
 	def print_intro
-		puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition."
+		print_by_letter('=========================================================')
+		puts '___________.____       _____    _________ ___ ___'
+    sleep 0.3
+		puts '\_   _____/|    |     /  _  \  /   _____//   |   \\'
+		sleep 0.3
+		puts ' |    __)  |    |    /  /_\  \ \_____  \/    ~    \\'
+		sleep 0.3
+		puts ' |     \   |    |___/    |    \/        \    Y    /'
+		sleep 0.3
+		puts ' \___  /   |_______ \____|__  /_______  /\___|_  / '
+		sleep 0.3
+		puts '     \/            \/       \/        \/       \/ '
+		sleep 0.3
+		print_by_letter "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition."
 	end
 
 	def prompt_question(question) #takes the question and prints it out. Returns input from the user
-		puts "Definition: "
-		puts "- #{question.capitalize}\n\n"
-		print "Guess: "
-		gets.chomp
+		print_by_letter "=========================================================\n\n"
+		print_by_letter "DEFINITION:\n"
+		print_by_letter "#{question.capitalize}\n\n"
 	end
 
-	def prompt_incorrect(question)
+	def get_input
+    print "GUESS: "
+    gets.chomp
+	end
+
+	def prompt_incorrect
 		puts "Incorrect! Try again.\n\n"
 	end
 
 	def prompt_too_many
-		puts "Incorrect! No more guesses available."		
+		puts "Incorrect! No more guesses available."
 	end
 
 	def prompt_correct_answer(answer)
-		puts "The correct answer: #{answer}"	
+		puts "The correct answer: #{answer}\n"
+		puts "=========================================================\n"
 	end
 
 	def prompt_correct
-		puts "Correct!"
+		puts "Correct!\n"
+		puts "=========================================================\n"
 	end
 
 	def print_stats(correct, incorrect)
